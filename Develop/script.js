668108ca09625f007d9bf3922107eae0d88f26fa
Veis1337 <117036853+Veis1337@users.noreply.github.com>
2022-12-03 12:28:22 -0500

    All variables defined, function mostly done.

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,18 @@
 
 //create generatePassword function
 function generatePassword() {
-  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@[\]^_`{|}`~`"
+  let upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  let lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let specialChars = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+  let numChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
   //Prompt the user upon button click for the password criteria
   let criteriaLength = confirm("Would you like to specify your own password length?");
   let criteriaUpper = confirm("Would you like to use Uppercase letters in your password?");   //  Lowercase, uppercase, numeric, and/or special characters?
   let criteriaLower = confirm("Would you like to use lowercase letters in your password?");
-  let criteriaSpecial = confirm("Would you like to use special characters (!@# etc) in your password?");
+  let criteriaSpecial = confirm("Would you like to use special characters (123!@# etc) in your password?");
+  let criteriaNumbers = confirm("Would you like to use numbers in your password?");
+  let passwordRange = [];
+  let passwordResult = [];
     if (criteriaLength) {
     //  Password length 8 < 128
   let passwordLength = prompt("How many characters would you like in your password?", "Number between 8 and 128");
@@ -21,8 +27,20 @@ function generatePassword() {
     console.log("User has not specified their own passwordLength, and so it will be ", passwordLength);
   }
 
-  for (let i = 0; i <= password.Length; i++) {
-    let passWord = Math.floor(Math.random() * chars.length);
+    if (criteriaUpper) {
+      passwordResult = passwordResult.concat(upperChars);
+    }
+    if (criteriaLower) {
+      passwordResult = passwordResult.concat(lowerChars);
+    }
+    if (criteriaSpecial) {
+      passwordResult = passwordResult.concat(specialChars);
+    }
+    if (criteriaNumbers) {
+      passwordResult = passwordResult.concat(numChars);
+    }
+  for (i = 0; i < password.Length; i++) {
+    
     
   }
     //Validate the input and make sure at least one character type has been selected
@@ -42,7 +60,7 @@ function generatePassword() {
   //  Return outputed password to user
 
 
-  return "Generated password will output here";
+  return "Generated password will output here";1
 }
 
 // Get references to the #generate element
