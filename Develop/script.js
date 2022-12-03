a817f5c3e79eecba436ec8a480e9d5150c381177
Veis1337 <117036853+Veis1337@users.noreply.github.com>
2022-12-03 14:20:23 -0500

    Finished function, final product works like a charm

--- Develop/script.js
+++ Develop/script.js
@@ -3,64 +3,56 @@
 //create generatePassword function
 function generatePassword() {
   let upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  let lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   let specialChars = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
   let numChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
   //Prompt the user upon button click for the password criteria
   let criteriaLength = confirm("Would you like to specify your own password length?");
   let criteriaUpper = confirm("Would you like to use Uppercase letters in your password?");   //  Lowercase, uppercase, numeric, and/or special characters?
   let criteriaLower = confirm("Would you like to use lowercase letters in your password?");
-  let criteriaSpecial = confirm("Would you like to use special characters (123!@# etc) in your password?");
+  let criteriaSpecial = confirm("Would you like to use special characters (!@# etc) in your password?");
   let criteriaNumbers = confirm("Would you like to use numbers in your password?");
   let passwordRange = [];
   let passwordResult = [];
-    if (criteriaLength) {
+
+  if (criteriaLength) {
     //  Password length 8 < 128
-  let passwordLength = prompt("How many characters would you like in your password?", "Number between 8 and 128");
-    if (passwordLength  > 7 && passwordLength < 129) {
+      passwordLength = prompt("How many characters would you like in your password?", "Number between 8 and 128");
+  if (passwordLength > 7 && passwordLength < 129) {
       console.log(passwordLength, "--Thank you, that input is indeed within those parameters");
     } else {
-      alert("Error: That value is not a # between 8 and 128");
+      alert("Error: That value is not a # between 8 and 128")
+      return;
     }
-  } else {
-    passwordLength = Math.floor(Math.random() * (128 - 8) + 8);
-    console.log("User has not specified their own passwordLength, and so it will be ", passwordLength);
+  }
+  if (!criteriaLength) {
+      passwordLength = Math.floor(Math.random() * (128 - 8) + 8);
+      console.log("User has not specified their own passwordLength, and so it will be ", passwordLength);
+    }
+
+
+  if (criteriaUpper) {
+    passwordResult = passwordResult.concat(upperChars);
+  }
+  if (criteriaLower) {
+    passwordResult = passwordResult.concat(lowerChars);
+  }
+  if (criteriaSpecial) {
+    passwordResult = passwordResult.concat(specialChars);
+  }
+  if (criteriaNumbers) {
+    passwordResult = passwordResult.concat(numChars);
+  }
+  if (!criteriaLength && !criteriaLower && !criteriaNumbers && !criteriaSpecial) {
+    alert("Please choose at least one option")   //Validate the input and make sure at least one character type has been selected
+    return
   }
 
-    if (criteriaUpper) {
-      passwordResult = passwordResult.concat(upperChars);
-    }
-    if (criteriaLower) {
-      passwordResult = passwordResult.concat(lowerChars);
-    }
-    if (criteriaSpecial) {
-      passwordResult = passwordResult.concat(specialChars);
-    }
-    if (criteriaNumbers) {
-      passwordResult = passwordResult.concat(numChars);
-    }
-  for (i = 0; i < password.Length; i++) {
-    
-    
+  for (let i = 0; i < passwordLength; i++) {
+    passwordRange.push (passwordResult[Math.floor(Math.random() * passwordResult.length)]);
   }
-    //Validate the input and make sure at least one character type has been selected
 
-
-
-
-  // let lowerCaseBoo = prompt("Would you like to use lowercase characters?")
-  // let lowerCase = "abcdefghijklmnopqrstuvwxyz"
-  // let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  // let specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}`~`"
-
-
-
-
-  //Generate password
-  //  Return outputed password to user
-
-
-  return "Generated password will output here";1
+  return passwordRange.join("");
 }
 
 // Get references to the #generate element
